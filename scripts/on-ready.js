a806311ce56ba72c6c2c60fdc809b310e282f352
Wouter Meuwis <woutermeuwis@gmail.com>
2025-05-04 15:06:46 +0200

    removed dependency for better V13 support

--- scripts/on-ready.js
+++ scripts/on-ready.js
@@ -1,39 +1,30 @@
 export function onReady() {
-    try {
-        window.Ardittristan.ColorSetting.tester;
-    } catch {
-        ui.notifications.notify(
-            'Please ensure the module "lib - ColorSettings" is enabled.',
-            'error',
-            { permanent: true }
-        );
-    }
-
-    new window.Ardittristan.ColorSetting("damage-progress", "HighHpColor",{
+    // define settings
+    game.settings.register("damage-progress", "HighHpColor", {
         name: "High HP Color",
         hint: "The color to tint tokens at high hp",
-        label: "Select High HP Color",
-        restricted: true,
-        defaultColor: '#D4FC0A',
-        scope: "world"
+        scope: "world",
+        config: true,
+        type: String,
+        default: '#D4FC0A',
     });
 
-    new window.Ardittristan.ColorSetting("damage-progress", "MidHpColor",{
+    game.settings.register("damage-progress", "MidHpColor", {
         name: "Mid HP Color",
         hint: "The color to tint tokens at mid hp",
-        label: "Select Mid HP Color",
-        restricted: true,
-        defaultColor: '#FCAB0A',
-        scope: "world"
+        scope: "world",
+        config: true,
+        type: String,
+        default: '#FCAB0A',
     });
 
-    new window.Ardittristan.ColorSetting("damage-progress", "LowHpColor",{
+    game.settings.register("damage-progress", "LowHpColor", {
         name: "Low HP Color",
         hint: "The color to tint tokens at low hp",
-        label: "Select Low HP Color",
-        restricted: true,
-        defaultColor: '#FC320A',
-        scope: "world"
+        scope: "world",
+        config: true,
+        type: String,
+        default: '#FC320A',
     });
 
     console.log("damage-progress | Settings registered!");
